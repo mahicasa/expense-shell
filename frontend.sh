cabd614f0897bbbcca0219ed09081906d7ad01c3
mahicasa <60077427+mahicasa@users.noreply.github.com>
2024-01-29 19:13:44 +0100

    Update frontend.sh

--- frontend.sh
+++ frontend.sh
@@ -1,10 +1,11 @@
 dnf install nginx -y #Install Nginx
-systemctl enable nginx
-systemctl start nginx
+cp expense.conf /etc/nginx/default.d/expense.conf
 
 rm -rf /usr/share/nginx/html/* #Remove the default content that web server is serving
-
 curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip #Download the frontend content#
 
 cd /usr/share/nginx/html #Extract the frontend content
 unzip /tmp/frontend.zip
+
+systemctl enable nginx
+systemctl start nginx
