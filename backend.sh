7d757419fd25e0ec96245437476c960b2073eff2
mahicasa <mahicasa@gmail.com>
2024-01-30 20:04:09 +0100

    Add backend configs

---
+++ backend.sh
@@ -0,0 +1,12 @@
+dnf module disable nodejs -y
+dnf module enable nodejs:18 -y
+dnf install nodejs -y
+
+useradd expense
+mkdir /app
+
+curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip
+cd /app
+unzip /tmp/backend.zip
+
+npm install
