40600626b90773f5ea50025e2c5dd6ba2ac3a645
mahicasa <mahicasa@gmail.com>
2024-02-10 16:45:16 +0100

    exit.sh

--- frontend.sh
+++ frontend.sh
@@ -1,11 +1,35 @@
-dnf install nginx -y #Install Nginx
-cp expense.conf /etc/nginx/default.d/expense.conf
+log_file=/tmp/expense.log
 
-rm -rf /usr/share/nginx/html/* #Remove the default content that web server is serving
-curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip #Download the frontend content#
+Head() {
+  echo -e "\e[36m$1\e[0m"
+}
 
-cd /usr/share/nginx/html #Extract the frontend content
+Head "Install Nginx"
+dnf install nginx -y &>>$log_file
+echo $?
+
+Head "Copy config"
+cp expense.conf /etc/nginx/default.d/expense.conf &>>$log_file
+echo $?
+
+Head "Remove the default content that web server is serving"
+rm -rf /usr/share/nginx/html/* &>>$log_file
+echo $?
+
+Head "Download the frontend content"
+curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip &>>$log_file
+echo $?
+
+Head "Extract the frontend content"
+cd /usr/share/nginx/html &>>$log_file
+echo $?
+
 unzip /tmp/frontend.zip
 
-systemctl enable nginx
-systemctl start nginx
+Head "Enable nginx"
+systemctl enable nginx &>>$log_file
+echo $?
+
+Head "Restart nginx"
+systemctl restart nginx &>>$log_file
+echo $?
