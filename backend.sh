85a607f82bd71381c8eaee5d38ffe4a7d7b30634
mahicasa <mahicasa@gmail.com>
2024-02-10 16:06:13 +0100

    exit.sh

--- backend.sh
+++ backend.sh
@@ -1,44 +1,48 @@
 MYSQL_PASSWORD=$1
 log_file=/tmp/expense.log
 
-echo Disable the default version of NodeJS
+Head() {
+  Head " -e "\e[36m$1\e[0m"
+}
+
+Head "Disable the default version of NodeJS"
 dnf module disable nodejs -y &>>$log_file
 
-echo module enable nodejs18 version
+Head "module enable nodejs18 version"
 dnf module enable nodejs:18 -y &>>$log_file
 
-echo install NodeJS
+Head "install NodeJS"
 dnf install nodejs -y &>>$log_file
 
-echo configure the backend service
+Head "configure the backend service"
 cp backend.service /etc/systemd/system/backend.service
 
-echo creating application useradd
+Head "creating application useradd"
 useradd expense
 
-echo remove the existing app content
+Head "remove the existing app content"
 rm -rf /app
 
-echo create new app directory
+Head "create new app directory"
 mkdir /app
 
-echo download the application content
+Head "download the application content"
 curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip &>>$log_file
 cd /app
 
-echo extracting application content
+Head "extracting application content"
 unzip /tmp/backend.zip &>>$log_file
 
-echo downloading the application dependencies
+Head "downloading the application dependencies"
 npm install &>>$log_file
 
 
-echo reloading the SystemD and Start Backend Service
+Head "reloading the SystemD and Start Backend Service"
 systemctl daemon-reload &>>$log_file
 systemctl enable backend &>>$log_file
 systemctl restart backend &>>$log_file
 
-echo install the MYSQL server
+Head "install the MYSQL server"
 dnf install mysql -y &>>$log_file
 mysql -h mysql-dev.rdevopsb73.online -uroot -p${MYSQL_PASSWORD} < /app/schema/backend.sql &>>$log_file
 # mysql -h mysql-dev.mahicasa.online -uroot -pExpenseApp@1 < /app/schema/backend.sql
