3cc5b22f6f08c94e9fc0f68a3fd8a4d0180d6c92
mahicasa <mahicasa@gmail.com>
2024-02-26 20:10:57 +0100

    If conditions

--- backend.sh
+++ backend.sh
@@ -6,88 +6,43 @@ source common.sh
 
 Head "Disable the default version of NodeJS"
 dnf module disable nodejs -y &>>$log_file
-if [ $? -eq 0 ]; then
-  echo SUCCESS
-else
-  echo FAILURE
-  exit 1
-fi
+Stat $?
 
 Head "module enable nodejs18 version"
 dnf module enable nodejs:18 -y &>>$log_file
-if [ $? -eq 0 ]; then
-  echo SUCCESS
-else
-  echo FAILURE
-  exit 1
-fi
+Stat $?
 
 Head "install NodeJS"
 dnf install nodejs -y &>>$log_file
-if [ $? -eq 0 ]; then
-  echo SUCCESS
-else
-  echo FAILURE
-  exit 1
-fi
+Stat $?
 
 Head "configure the backend service"
 cp backend.service /etc/systemd/system/backend.service
-if [ $? -eq 0 ]; then
-  echo SUCCESS
-else
-  echo FAILURE
-  exit 1
-fi
+Stat $?
 
 Head "creating application useradd"
 useradd expense
-if [ $? -eq 0 ]; then
-  echo SUCCESS
-else
-  echo FAILURE
-  exit 1
-fi
+Stat $?
 
 App_Prereq "/app"
 
 Head "downloading the application dependencies"
 npm install &>>$log_file
-if [ $? -eq 0 ]; then
-  echo SUCCESS
-else
-  echo FAILURE
-  exit 1
-fi
+Stat $?
 
 Head "reloading the SystemD and Start Backend Service"
 systemctl daemon-reload &>>$log_file
 systemctl enable backend &>>$log_file
 systemctl restart backend &>>$log_file
-if [ $? -eq 0 ]; then
-  echo SUCCESS
-else
-  echo FAILURE
-  exit 1
-fi
+Stat $?
 
 Head "Install MySQL Client"
 dnf install mysql -y &>>$log_file
-if [ $? -eq 0 ]; then
-  echo SUCCESS
-else
-  echo FAILURE
-  exit 1
-fi
+Stat $?
 
 Head "Load Schema"
 mysql -h mysql-dev.rdevopsb73.online -uroot -p${MYSQL_PASSWORD} < /app/schema/backend.sql &>>$log_file
-if [ $? -eq 0 ]; then
-  echo SUCCESS
-else
-  echo FAILURE
-  exit 1
-fi
+Stat $?
 
 
 # mysql -h mysql-dev.mahicasa.online -uroot -pExpenseApp@1 < /app/schema/backend.sql
--- frontend.sh
+++ frontend.sh
@@ -4,30 +4,16 @@ source common.sh
 
 Head "Install Nginx"
 dnf install nginx -y &>>$log_file
-if [ $? -eq 0 ]; then
-  echo SUCCESS
-else
-  echo FAILURE
-  exit 1
-fi
+Stat $?
 
 Head "Copy config"
 cp expense.conf /etc/nginx/default.d/expense.conf &>>$log_file
-if [ $? -eq 0 ]; then
-  echo SUCCESS
-else
-  echo FAILURE
-  exit 1
-fi
+Stat $?
 
 App_Prereq "/usr/share/nginx/html"
 
 Head "Start nginx service"
 systemctl enable nginx &>>$log_file
 systemctl restart nginx &>>$log_file
-if [ $? -eq 0 ]; then
-  echo SUCCESS
-else
-  echo FAILURE
-  exit 1
-fi
+Stat $?
+
